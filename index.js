50010d16c94e06e28580c1c3c6cd941f9eb63fb6
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 04:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const logDriverNames = function (drivers) {
 }
 
 const logDriversByHometown = function (drivers, location) {
-  drivers.filter(driver => driver.hometown == location).logDriverNames
+  drivers.logDriverNames(drivers.filter(driver => driver.hometown == location))
 }
